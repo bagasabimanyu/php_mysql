ab3a2fdf0a52a146420339f47fa908ee2bb1d3cc
bagasabimanyu <2020210443@students.perbanas.ac.id>
2022-10-24 11:03:24 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+echo "Selamat Datang <br>";
+$nama = "Bagas Abimanyu";
+$umur = 20;
+
+echo "Nama saya adalah <strong>$nama</strong>dan umur saya adalah <strong>$umur</strong> tahun<br>";
+
+
+$namaAdik = "Salma";
+$umurAdik = 18;
+
+echo "Nama adik saya adalah <strong>$namaAdik</strong> dan umur adik saya adalah <strong>$umurAdik</strong> tahun<br>";
+
+$selisih = $umur - $umurAdik;
+echo "Selisih usia saya dan adik saya adalah <strong>$selisih</strong> tahun<br>";
+
+$jumlah = $umur + $umurAdik;
+echo "Jumlah usia saya dan adik saya adalah <strong>$jumlah</strong> tahun<br>";
+
+$hasilBagi = $umur / $umurAdik;
+echo "Hasil bagi usia saya dan adik saya adalah <strong>$hasilBagi</strong> tahun<br>";
+
+$hasilKali = $umur * $umurAdik;
+echo "Hasil kali usia saya dan adik saya adalah <strong>$hasilKali</strong> tahun<br>";
+
+
+
+
+?>
